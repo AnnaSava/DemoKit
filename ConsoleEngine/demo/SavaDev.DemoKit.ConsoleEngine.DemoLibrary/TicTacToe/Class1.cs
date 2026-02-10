75dc3a78e31f65fe378a3567b1522113f40cebc4
Anna Savina <anna.s.savina@gmail.com>
2026-02-11 01:13:35 +0300

    New SavaDev.DemoKit.ConsoleEngine.DemoLibrary project

---
+++ ConsoleEngine/demo/SavaDev.DemoKit.ConsoleEngine.DemoLibrary/TicTacToe/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavaDev.DemoKit.ConsoleEngine.DemoLibrary.TicTacToe
+{
+    internal class Class1
+    {
+    }
+}
